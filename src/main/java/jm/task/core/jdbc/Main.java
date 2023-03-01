f5961f8e7d0ef32417ab6cd8e2fd3322564933c7
laker <laker9915@mail.ru>
2023-03-01 20:02:59 +0300

    Fix bug

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,15 +1,7 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import javax.xml.transform.Result;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -19,7 +11,7 @@ public class Main {
         USI.saveUser("Sergey", "Vlasov", (byte) 20);
         USI.saveUser("Vadim", "Kireev", (byte) 19);
         USI.saveUser("Alex", "Arifulin", (byte) 24);
-        USI.getAllUsers();
+        System.out.println(USI.getAllUsers());
         USI.cleanUsersTable();
         USI.dropUsersTable();
     }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -14,7 +14,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
         try (Connection connection = connectionManager.getConnection(); Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users (id BIGINT AUTO_INCREMENT PRIMARY KEY, " +
                             "name VARCHAR(50) NOT NULL, " +
                             "lastName VARCHAR(100) NOT NULL, " +
                             "age INT NOT NULL);");
@@ -33,9 +33,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) {
         try (Connection connection = connectionManager.getConnection(); PreparedStatement statement = connection.prepareStatement(
-                "INSERT users(name, lastname, age) VALUES (?, ?, ?);")) {
+                "INSERT users(name, lastName, age) VALUES (?, ?, ?);")) {
             statement.setString(1, name);
-            statement.setString(2, name);
+            statement.setString(2, lastName);
             statement.setByte(3, age);
             statement.executeUpdate();
             System.out.println(String.format("User с именем – %s добавлен в базу данных;", name));
@@ -55,19 +55,19 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public List<User> getAllUsers() {
-        List<User> userList= new ArrayList<>();
+        List<User> mvpClub= new ArrayList<>();
         try (Connection connection = connectionManager.getConnection();
              PreparedStatement statement = connection.prepareStatement("SELECT * FROM users;");
              ResultSet resultSet = statement.executeQuery()) {
             while (resultSet.next()) {
                 User user = new User(resultSet.getString("name"), resultSet.getString("lastName"),
                         resultSet.getByte("age"));
-                userList.add(user);
+                mvpClub.add(user);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return userList;
+        return mvpClub;
     }
 
     public void cleanUsersTable() {
